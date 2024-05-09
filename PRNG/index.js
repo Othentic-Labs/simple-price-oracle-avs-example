a2a5d8cbe94d9eb1de15c45292841ecd2343ca30
Yotam Bar-On <yotam.bar-on@othentic.xyz>
2024-05-09 21:01:43 +0300

    PRNG AVS: fix leader election implementation

--- PRNG/index.js
+++ PRNG/index.js
@@ -12,7 +12,8 @@ const app = express();
 const port = 4002;
 app.use(express.json());
 
-const rpcUrl = 'https://rpc.ankr.com/eth_holesky/80921f6657bf5ad15fb4577b369c28c4f84a652625322575f085fceaa7e47653';
+// L2 RPC provider
+const rpcUrl = process.env.L2_RPC;
 const provider = new ethers.JsonRpcProvider(rpcUrl);
 
 // The AttestationCenter contract object
@@ -23,29 +24,33 @@ const attestationCenterAbi = [
 ];
 const attestationCenterContract = new ethers.Contract(attestationCenterAddress, attestationCenterAbi, provider);
 
-let currentPerformer;
+/**
+  * Find the elected task performer for a certain block
+  */
+async function electedLeader (blockNumber) {
+    const count = await attestationCenterContract.numOfOperators({ blockTag: blockNumber });
+    const selectedOperatorId = (BigInt(blockNumber) % count) + 1n;
+    const paymentDetails = await attestationCenterContract.operators(selectedOperatorId, { blockTag: blockNumber });
+    return paymentDetails[0];
+}
 
 /**
   * Performing tasks:
   * The "Task Performer" is chosen in a "Round Robin" fashion, meaning the
-  * operators perform tasks in the order of their IDs: 0, 1, 2, ... etc.
+  * operators perform tasks in the order of their IDs: 1, 2, 3, ... etc.
   *
   * The round-robin scheme is trivially implemented by taking the block
-  * number modulo the number of operators. This gives us a number in the range
-  * [0..count-1], which we use as the ID of the chosen performer.
+  * number modulo the number of operators (plus 1). This gives us a number in
+  * the range [1..count], which we use as the ID of the chosen performer.
   */
 provider.on('block', async (blockNumber) => {
-    // Every operator knows who is supposed to send a task in the next 12 seconds
-    const count = await attestationCenterContract.numOfOperators();
-    const selectedOperatorId = BigInt(blockNumber) % count;
-    const paymentDetails = await attestationCenterContract.operators(selectedOperatorId);
-
-    currentPerformer = paymentDetails[0];
+    // Every operator knows who is supposed to send a task in the next block
+    const currentPerformer = await electedLeader(blockNumber);
 
     // If the current performer is the operator itself, it performs the task
     if (currentPerformer === nodeAccount.address) {
         console.log(`Performing task for block ${blockNumber}...`);
-        const proofOfTask = Date.now().toString();
+        const proofOfTask = `${blockNumber}+${Date.now()}`;
         const taskDefinitionId = 0;
         const message = ethers.AbiCoder
             .defaultAbiCoder()
@@ -64,31 +69,6 @@ provider.on('block', async (blockNumber) => {
             proofOfTask, taskDefinitionId, nodeAccount.address, sig
         ]);
     }
-    // const count = await attestationCenterContract.numOfOperators();
-    // const selectedOperatorId = BigInt(blockNumber) % count;
-    // const paymentDetails = await attestationCenterContract.operators(selectedOperatorId);
-    //
-    // currentPerformer = paymentDetails[0];
-    //
-    // if (currentPerformer === nodeAccount.address) {
-    //     console.log(`Performing task for block ${blockNumber}...`);
-    //     const proofOfTask = Date.now().toString();
-    //     const taskDefinitionId = 0;
-    //     const message = ethers.AbiCoder
-    //         .defaultAbiCoder()
-    //         .encode(
-    //             ["string", "address", "uint16"],
-    //             [proofOfTask, nodeAccount.address, taskDefinitionId]
-    //         );
-    //     const messageHash = ethers.keccak256(message);
-    //     const sig = nodeAccount.signingKey.sign(messageHash).serialized;
-    //
-    //     console.log(`Performing task with seed: ${proofOfTask}`);
-    //
-    //     new ethers.JsonRpcProvider(NODE_RPC).send('sendTask', [
-    //         proofOfTask, taskDefinitionId, nodeAccount.address, sig
-    //     ]);
-    // }
 });
 
 /**
@@ -97,9 +77,14 @@ provider.on('block', async (blockNumber) => {
   * the correct performer. It receives the performer from the Othentic node
   * and checks that it's the `currentPerformer`.
   */
-app.post('/task/validate', (req, res) => {
-    const performer = req.body.performer;
-    let isValid = performer === currentPerformer;
+app.post('/task/validate', async (req, res) => {
+    const { proofOfTask, performer } = req.body;
+    const blockNumber = parseInt(proofOfTask.split('+')[0], 10); // Extract the block number from the proof of task
+    const electedPerformer = await electedLeader(blockNumber); // Get the elected performer for that block
+
+    console.log(`Validating task for block number: ${blockNumber}, Task Performer: ${performer}, Elected Performer: ${electedPerformer}`)
+
+    let isValid = performer === electedPerformer; // Verify the performer is the elected performer
 
     res.status(200);
     res.json({
@@ -110,15 +95,3 @@ app.post('/task/validate', (req, res) => {
 });
 
 app.listen(port, () => { console.log(`AVS Implementation listening on localhost:${port}`); });
-
-// Utility function for signing the proof-of-task
-function sign (proofOfTask, taskDefinitionId) {
-    const message = ethers.AbiCoder
-        .defaultAbiCoder()
-        .encode(
-            ["string", "address", "uint16"],
-            [proofOfTask, nodeAccount.address, taskDefinitionId]
-        );
-    const messageHash = ethers.keccak256(message);
-    return nodeAccount.signingKey.sign(messageHash).serialized;
-}
