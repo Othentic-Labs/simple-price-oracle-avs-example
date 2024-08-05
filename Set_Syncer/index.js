e980486c1ae07508a5d05df24dab203bb6ec5e69
Tom Azoulay Othentic <tom@Toms-MacBook-Pro.local>
2024-08-05 14:17:12 +0300

    refactor variable name

--- Set_Syncer/index.js
+++ Set_Syncer/index.js
@@ -2,11 +2,11 @@ const execSync = require('child_process').execSync;
 const { ethers } = require('ethers');
 const dotenv = require('dotenv');
 dotenv.config();
-let private_key_syncer = process.env.PRIVATE_KEY_SYNCER;
-if (!private_key_syncer.startsWith("0x")) {
-    private_key_syncer = "0x" + private_key_syncer;
+let privateKeySyncer = process.env.PRIVATE_KEY_SYNCER;
+if (!privateKeySyncer.startsWith("0x")) {
+    privateKeySyncer = "0x" + privateKeySyncer;
 }
-const ethersAddress = ethers.computeAddress(private_key_syncer);
+const ethersAddress = ethers.computeAddress(privateKeySyncer);
 process.env.PRIVATE_KEY = process.env.PRIVATE_KEY_DEPLOYER;
 const output = execSync(`othentic-cli network set-syncer --syncer-address ${ethersAddress}`, { encoding: 'utf-8'});
 console.log(output);
