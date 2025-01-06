18e37d5dee583255ea2500a04fff7fde108a907f
Dima-Othentic <dimar@othentic.xyz>
2025-01-06 21:00:52 +0200

    Update for variable name to reduce discrepancy.

--- Execution_Service/src/dal.service.js
+++ Execution_Service/src/dal.service.js
@@ -13,7 +13,7 @@ function init() {
   pinataApiKey = process.env.PINATA_API_KEY;
   pinataSecretApiKey = process.env.PINATA_SECRET_API_KEY;
   rpcBaseAddress = process.env.OTHENTIC_CLIENT_RPC_ADDRESS;
-  privateKey = process.env.PRIVATE_KEY;
+  privateKey = process.env.PRIVATE_KEY_PERFORMER;
 }
 
 async function sendTask(proofOfTask, data, taskDefinitionId) {
