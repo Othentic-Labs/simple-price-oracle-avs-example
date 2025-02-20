85f3b1ec888145374a1ab83ea6af1fe0abe405f4
ivank <>
2025-02-20 15:55:18 +0100

    fix: Fix deployment script

--- contracts/script/DynamicFeesAvsHook.s.sol
+++ contracts/script/DynamicFeesAvsHook.s.sol
@@ -19,6 +19,9 @@ import {Script, console} from "forge-std/Script.sol";
 import {IAttestationCenter} from "../src/interfaces/IAttestationCenter.sol";
 import {DynamicFeesAvsHook} from "../src/DynamicFeesAvsHook.sol";
 import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
+import {Hooks} from "v4-core/src/libraries/Hooks.sol";
+import {HookMiner} from "v4-periphery/src/utils/HookMiner.sol";
+import {console} from "forge-std/console.sol";
 
 // How to:
 // Either `source ../../.env` or replace variables in command.
@@ -29,8 +32,24 @@ contract DynamicFeesAvsHookDeploy is Script {
     function setUp() public {}
 
     function run(address attestationCenter, address poolManager) public {
+        // https://book.getfoundry.sh/guides/deterministic-deployments-using-create2?highlight=CREATE2_DEPLOY#deterministic-deployments-using-create2
+        address CREATE2_DEPLOYER = 0x4e59b44847b379578588920cA78FbF26c0B4956C;
+
+        uint160 flags = uint160(Hooks.AFTER_INITIALIZE_FLAG | Hooks.BEFORE_SWAP_FLAG);
+        bytes memory constructorArgs = abi.encode(attestationCenter, IPoolManager(poolManager));
+
+        (address hookAddress, bytes32 salt) =
+            HookMiner.find(CREATE2_DEPLOYER, flags, type(DynamicFeesAvsHook).creationCode, constructorArgs);
+
+        console.log("Mined hook address:", hookAddress);
+        console.log("Salt:", vm.toString(salt));
+
         vm.startBroadcast();
-        DynamicFeesAvsHook avsHook = new DynamicFeesAvsHook(attestationCenter, IPoolManager(poolManager));
+        DynamicFeesAvsHook avsHook = new DynamicFeesAvsHook{salt: salt}(attestationCenter, IPoolManager(poolManager));
+
+        require(address(avsHook) == hookAddress, "Hook address mismatch");
+
         IAttestationCenter(attestationCenter).setAvsLogic(address(avsHook));
+        vm.stopBroadcast();
     }
 }
