119e7482e440f7a4a2e91cbb3976bf33fd9d1f80
himanshugarg06 <himanshug@othentic.xyz>
2025-06-23 16:51:51 +0530

    Add docker compose for different slashing conditions

--- Validation_Service/src/validator.service.js
+++ Validation_Service/src/validator.service.js
@@ -2,18 +2,15 @@ require('dotenv').config();
 const dalService = require("./dal.service");
 const oracleService = require("./oracle.service");
 
-async function validate(proofOfTask) {
+var validationResult = true;
+console.log("validator.service.js loaded");
 
+async function validate(proofOfTask) {
+  console.log("In the validate function");
+  validationResult = process.env.VALIDATOR_RESULT === "true";
   try {
-      const taskResult = await dalService.getIPfsTask(proofOfTask);
-      var data = await oracleService.getPrice("ETHUSDT");
-      const upperBound = data.price * 1.05;
-      const lowerBound = data.price * 0.95;
-      let isApproved = true;
-      if (taskResult.price > upperBound || taskResult.price < lowerBound) {
-        isApproved = false;
-      }
-      return isApproved;
+      console.log("Validation Service started which returns ", validationResult);
+      return validationResult;
     } catch (err) {
       console.error(err?.message);
       return false;
